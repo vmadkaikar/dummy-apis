e8e7ac5d4eaba38d278a75a735b765beafbf353e
unknown <madkaikv@SUNMR820GX112C1.nala.roche.com>
2016-01-04 01:04:58 -0800

    Added inDate & outDate

--- api/data/data.controller.js
+++ api/data/data.controller.js
@@ -10,11 +10,18 @@ exports.getData = function(request, response) {
         name = ['Tom', 'Sam', 'Alex', 'Bob', 'Peter', 'Jon', 'John', 'Max', 'Bill', 'Bonney'],
         lastName = ['Mc Donald', 'Jhonson', 'Martin', 'Patel', 'Bush', 'Doe', 'Hanks', 'Yu', 'Hog', 'Singh'];
     for(var i=0; i<count; i++) {
+        var d = new Date(),
+            startDate = i%4==0 ? new Date(d.setDate(d.getDate() - Math.round(Math.random()*10))) : new Date(d.setDate(d.getDate()-Math.round((Math.random() * 50) + 50))),
+            endDate = new Date(startDate.getTime());
+
+        endDate = i%4==0 ? null : new Date(endDate.setDate(endDate.getDate() + Math.round(Math.random()*10)));
         responseObj.data.push({
             name: 'Mr. ' + name[Math.round(10 * Math.random()) -1] + ' ' + lastName[Math.round(10 * Math.random()) -1],
             age: Math.round(100 * Math.random()),
             MRN: shortid.generate(),
             roomNumber: Math.round(500 * Math.random()),
+            inDate: startDate.getTime(),
+            outDate: endDate ? endDate.getTime() : endDate,
             vitals: {
                 heartRate: 60 + Math.round(80 * Math.random()),
                 bloodPressureSystolic: 90 + Math.round(80 * Math.random()),
